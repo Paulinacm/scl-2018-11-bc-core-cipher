6e31bb6765d199d0ddffa489c41645c9890bc797
Paulinacm <pauly.cm@gmail.com>
2018-12-05 12:41:56 -0300

    se modificó de function a addEventListener

--- src/index.js
+++ src/index.js
@@ -1,23 +1,21 @@
-/* Acá va tu código */
-let ingresoTexto = document.getElementById("textoCifrar");
-let nroOffsetCifrado = document.getElementById("offsetCifrado");
-let textoCifrado = document.getElementById("textoCifrado");
-let ingresoTexto1 = document.getElementById("textoDescifrar");
-let textoDescifrado = document.getElementById("textoDescifrado");
-let nroOffsetDescifrado = document.getElementById("offsetDescifrado")
 
-function cifrarTexto() {
-  if ((nroOffsetCifrado.value.length > 0 && !isNaN(nroOffsetCifrado.value)) && (ingresoTexto.value.length > 0)) {
-    textoCifrado.value = cipher.encode(ingresoTexto.value, nroOffsetCifrado.value);
-  } else {
-    // Si no se cumplen las condiciones, se resetea el campo del texto cifrado 
-    textoCifrado.value = "";
+  let ingresoTexto = document.getElementById("textoCifrar");
+  let nroOffsetCifrado = document.getElementById("offsetCifrado");
+  let textoCifrado = document.getElementById("textoCifrado");
+  let ingresoTexto1 = document.getElementById("textoDescifrar");
+  let textoDescifrado = document.getElementById("textoDescifrado");
+  let nroOffsetDescifrado = document.getElementById("offsetDescifrado");
+
+  ingresoTexto.addEventListener("onkeyup", nroOffsetCifrado);
+    if ((nroOffsetCifrado.value.length > 0 && !isNaN(nroOffsetCifrado.value)) && (ingresoTexto.value.length > 0)) {
+      textoCifrado.value = cipher.encode(ingresoTexto.value, nroOffsetCifrado.value);
+    } else {
+      textoCifrado.value = "";
+    }
+
+  ingresoTexto1.addEventListener("onkeyup", nroOffsetDescifrado);
+    if ((nroOffsetDescifrado.value.length > 0 && !isNaN(nroOffsetDescifrado.value)) && (ingresoTexto1.value.length > 0)) {
+      textoDescifrado.value = cipher.decode(ingresoTexto1.value, nroOffsetDescifrado.value);
+    } else {
+      textoDescifrado.value = "";
   }
-}
-function descifrarTexto() {
-  if ((nroOffsetDescifrado.value.length > 0 && !isNaN(nroOffsetDescifrado.value)) && (ingresoTexto1.value.length > 0)) {
-    textoDescifrado.value = cipher.decode(ingresoTexto1.value, nroOffsetDescifrado.value);
-  } else {
-    textoDescifrado.value = "";
-  }
-}
